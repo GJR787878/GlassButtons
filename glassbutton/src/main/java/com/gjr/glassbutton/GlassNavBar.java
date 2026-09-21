0dbdd459bec87017619cfc125b00657924026775
GJR787878 <GJR787878@users.noreply.github.com>
2026-09-21 21:04:30 +0800

    导航栏背景改半透明玻璃效果(20%不透明度)

--- glassbutton/src/main/java/com/gjr/glassbutton/GlassNavBar.java
+++ glassbutton/src/main/java/com/gjr/glassbutton/GlassNavBar.java
@@ -265,9 +265,10 @@ public class GlassNavBar extends FrameLayout {
     private void applyGlassBackground() {
         float density = getResources().getDisplayMetrics().density;
         GradientDrawable bg = new GradientDrawable();
-        bg.setColor(GlassButtonStyle.COLOR_NAV_BG);
+        // 半透明玻璃效果：20% 不透明度，内容可穿透
+        bg.setColor(0x331C1C1E);
         bg.setCornerRadius(Math.round(mCornerRadiusDp * density));
-        bg.setStroke(Math.round(1 * density), GlassButtonStyle.COLOR_NAV_BORDER);
+        bg.setStroke(Math.round(1 * density), 0x40FFFFFF);
         setBackground(bg);
     }
 }
